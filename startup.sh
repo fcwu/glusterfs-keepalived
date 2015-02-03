24de9421a5876ef7cef7d1d151fe9a3479e87a00
Doro Wu <fcwu.tw@gmail.com>
2015-02-03 15:38:23 +0800

    Add environment MP and IFACE

--- startup.sh
+++ startup.sh
@@ -10,8 +10,20 @@
 
 grep -q "#PUBLIC_IP#" /etc/keepalived/keepalived.conf \
     && {
-        [ -z "$VIP" ] && exit 0
-        sed -i "s/#PUBLIC_IP#/$VIP/" /etc/keepalived/keepalived.conf
+        [ -n "$VIP" ] && \
+            sed -i "s/#PUBLIC_IP#/$VIP/" /etc/keepalived/keepalived.conf
     }
+
+grep -q "#PUBLIC_IFACE#" /etc/keepalived/keepalived.conf \
+    && {
+        IFACE=${IFACE:-eth0}
+        sed -i "s/#PUBLIC_IFACE#/$IFACE/" /etc/keepalived/keepalived.conf
+    }
+
+
+[ -n "$MP" ] && {
+    setfattr -x trusted.glusterfs.volume-id $MP >/dev/null 2>&1
+    setfattr -x trusted.gfid $MP >/dev/null 2>&1
+}
 
 /usr/bin/supervisord -n
